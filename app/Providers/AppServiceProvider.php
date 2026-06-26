29be2fba2c53fc9ce0bc65e1418e37a2780fe32d
muh daffa <dafanasuyion@gmail.com>
2026-06-27 02:32:52 +0700

    Fix mixed content issue by forcing HTTPS and trusting all proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            URL::forceScheme('https');
+        } elseif (str_contains(request()->getHost(), 'hf.space')) {
+            URL::forceScheme('https');
+        }
     }
 }
